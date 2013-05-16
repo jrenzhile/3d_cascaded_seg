e39eb30fbe2991f36481d470640efaa636d3870e
luvegood <jrenzhile@gmail.com>
2013-05-16 16:52:25 +0800

    0516_minorchange

--- super_patch/sp_all.m
+++ super_patch/sp_all.m
@@ -17,15 +17,16 @@ mod_to_proc = mod_to_proc(3:end);
 
 
 for i=1:length(mod_to_proc)
+  [~,mod_name,~] = fileparts(mod_to_proc(i).name);
   if exist(sprintf('%s/%s_%d.mat',resdir,mod_name,sp_num),'file')==2
-      fprintf('%d Already Done', mod_to_proc(i));
+      fprintf('%d Already Done\n', i);
       continue;
   end
   time_spent_i = tic;
   [vertex,face] = read_off(sprintf('%s',mod_to_proc(i).name));
 
   seginfo = sp_main(face, vertex,sp_num , verbose); 
-  [~,mod_name,~] = fileparts(mod_to_proc(i).name);
+  
   save(sprintf('%s/%s_%d.mat',resdir,mod_name,sp_num),'seginfo','vertex','face');
   fprintf('Processing %.2f%%, time spent: %.2fs\n',i/length(mod_to_proc)*100, toc(time_spent_i));
 end
