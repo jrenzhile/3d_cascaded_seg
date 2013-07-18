3e3806652f321897033517e27a43dd2e064c8a62
luvegood <jrenzhile@gmail.com>
2013-07-18 19:02:59 +0800

    minor change

--- features/mex/getNeighbor.m
+++ features/mex/getNeighbor.m
@@ -3,6 +3,8 @@
 %
 % Given the face of the model, return the neighboring pairs.
 % Each row j in coloum i represent a neighboring face pair (i,j)
+% (Every face should have 3 neighbors, so size(neighbors, 1) = 3 in most
+% cases)
 % 
 % Zhile Ren <jrenzhile@gmail.com>
 % Mar, 2013
