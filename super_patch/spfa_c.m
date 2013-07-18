6fe2ce8f68e785968d12565b510cd8a203436808
luvegood <jrenzhile@gmail.com>
2013-07-18 17:59:03 +0800

    proposals

---
+++ super_patch/spfa_c.m
@@ -0,0 +1,7 @@
+% function minDist = spfa_c(neighbors, dist, v)
+% Given neighbors and distance matrix, 
+% perform Shortest Path Faster Algorithm(SPFA) algorithm in finding the
+% shortest path from v to every other faces.
+% 
+% Zhile Ren<jrenzhile@gmail.com>
+% Apr, 2013
