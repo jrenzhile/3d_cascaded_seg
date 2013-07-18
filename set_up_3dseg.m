8b5d5a808900ecd37aa9019689b71b8224ec4b4e
luvegood <jrenzhile@gmail.com>
2013-07-18 16:01:08 +0800

    0718_minor

--- set_up_3dseg.m
+++ set_up_3dseg.m
@@ -8,7 +8,9 @@ addpath(genpath(fullfile(pwd,'cascade_seg')));
 addpath(genpath(fullfile(pwd,'measures')));
 addpath(genpath(fullfile(pwd,'Descriptors')));
 addpath(genpath(fullfile(pwd,'others')));
-
+addpath(genpath(fullfile(pwd,'../toolbox_graph')));
+addpath(genpath(fullfile(pwd,'../minFunc')));
+addpath(pwd);
 % addpath(genpath('C:\Users\luvegood\Documents\MATLAB\minFunc\minFunc'));
 fprintf('Path set up done!\n');
 
